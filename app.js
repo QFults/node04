8e781aa9a56f30613797ec483ffe23d8025452e5
Quinton Fults <coding.banjo@gmail.com>
2020-05-14 14:02:59 -0700

    code

--- app.js
+++ app.js
@@ -14,30 +14,67 @@
 // cat.makeSound()
 // giraffe.makeSound()
 
-function Person (name, age, email, address) {
-  this.name = name
-  this.age = age
-  this.email = email
-  this.address = address
-  this.printInfo = () => {
-    console.log(`
-      Name: ${this.name}
-      Age: ${this.age}
-      Email: ${this.email}
-      Address: ${this.address}
-    `)
+class Animal {
+  constructor (type, sound) {
+    this.type = type
+    this.sound = sound
+    this.makeSound = () => {
+      console.log(this.sound)
+    }
   }
 }
 
-let person1 = new Person('John Doe', 54, 'johndoe@gmail.com', '123 Main St')
+let dog = new Animal('dog', 'bark')
 
-person1.printInfo()
+dog.makeSound()
 
-function Teacher (name, age, email, address, subject) {
-  Person.call(this, name, age, email, address)
-  this.subject = subject
+class Reptile extends Animal {
+  constructor(type, sound, scales) {
+    super(type, sound)
+    this.scales = scales
+  }
 }
 
-let teacher1 = new Teacher('Mr Johnson', 54, 'jjohnson@gmail.com', '321 South St', 'math')
+let lizard = new Reptile('lizard', 'hiss', 100)
 
-console.log(teacher1)
+console.log(lizard)
+
+
+// function Person (name, age, email, address) {
+//   this.name = name
+//   this.age = age
+//   this.email = email
+//   this.address = address
+//   this.printInfo = () => {
+//     console.log(`
+//       Name: ${this.name}
+//       Age: ${this.age}
+//       Email: ${this.email}
+//       Address: ${this.address}
+//     `)
+//   }
+// }
+
+// let person1 = new Person('John Doe', 54, 'johndoe@gmail.com', '123 Main St')
+
+// person1.printInfo()
+
+// function Teacher (name, age, email, address, subject) {
+//   Person.call(this, name, age, email, address)
+//   this.subject = subject
+// }
+
+// let teacher1 = new Teacher('Mr Johnson', 54, 'jjohnson@gmail.com', '321 South St', 'math')
+
+// console.log(teacher1)
+
+// function Student (name, age, email, address, gpa, grade, favoriteSubject) {
+//   Person.call(this, name, age, email, address)
+//   this.gpa = gpa
+//   this.grade = grade
+//   this.favoriteSubject = favoriteSubject
+// }
+
+// let student1 = new Student('Jack Doe', 18, 'jackdoe@gmail.com', '123 West St', 3.8, 12, 'english')
+
+// console.log(student1)
